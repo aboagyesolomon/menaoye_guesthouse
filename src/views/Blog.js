afd12bd4e5cc584301e0065df06e13c5cc728ba4
aboagyesolomon <solomonaboagye47@gmail.com>
2025-06-16 15:27:23 +0000

    fresh

--- src/views/Blog.js
+++ src/views/Blog.js
@@ -65,7 +65,7 @@ const Blog = () => {
                      </div>
                   </div>
                </div> */}
-               <ComingSoon/>
+               <ComingSoon />
             </div>
          </div>
       </div>
